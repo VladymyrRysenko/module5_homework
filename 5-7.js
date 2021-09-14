bc3de24a040ce79b0f54b1be242c88f5ef2d8df6
VladymyrRysenko <nva@centre.kharkov.ua>
2021-09-14 22:56:24 +0300

    Update 5-7.js

--- 5-7.js
+++ 5-7.js
@@ -1,1 +1,8 @@
+let array = [1, 2, 5, 10, null, 99, 101];
+let oddArray = array.filter(array => array % 2 == 0 && array !== null);
+let evenArray = array.filter(array => array % 2 !== 0 && array !== null);
+let nullArray = array.filter(array => !array);
+console.log(oddArray.length + "-кол-во четных");
+console.log(evenArray.length + "-кол-во нечетных");
+console.log(nullArray)
 
